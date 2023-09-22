b477ab7d798ca43ed75b22bbbd351fd4be5d6a65
Joaquim <devjoaquim@gmail.com>
2023-09-22 02:01:27 +0200

    Day 21, Part 2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -62,45 +62,90 @@ fn run1(input: &str) -> u64 {
     }
 }
 
+const ROLLS: [(u32,u128); 7] = [(3,1), (4,3), (5,6), (6,7), (7,6), (8,3), (9,1)];
+const WIN_SCORE: u32 = 21;
+
+// Return HashMap with keys: turns and values: HashMap with keys: score, and values: universes
+fn compute_universes(initial_position: u32) -> HashMap<u32, HashMap<u32, u128>> {
+    let mut result = HashMap::new();
+    let mut state = HashMap::from([((initial_position, 0), 1)]);
+    let mut turn = 1;
+    let mut all_win = false;
+    while !all_win {
+        all_win = true;
+        let mut rest = HashMap::new();
+        let mut next_state = HashMap::new();
+
+        for ((pos, score), universes) in state {
+            if score < WIN_SCORE {
+                for (x, u) in ROLLS {
+                    let new_pos = ((pos - 1 + x) % 10) + 1;
+                    let new_score = score + new_pos;
+                    all_win &= new_score >= WIN_SCORE;
+                    let new_universes = u*universes;
+                    if let Some(u2) = next_state.get_mut(&(new_pos, new_score)) {
+                        *u2 += new_universes;
+                    } else {
+                        next_state.insert((new_pos, new_score), new_universes);
+                    }
+                    if let Some(u2) = rest.get_mut(&new_score) {
+                        *u2 += new_universes;
+                    } else {
+                        rest.insert(new_score, new_universes);
+                    }
+                }
+            }
+        }
+        result.insert(turn, rest);
+        state = next_state;
+        turn += 1;
+    }
+
+    result
+}
+
 fn run2(input: &str) -> u128 {
     let (s1, s2) = input.split_once('\n').unwrap();
-    let s1 = s1.rsplit_once(' ').unwrap().1;
-    let pos1: u32 = s1.parse().unwrap();
-    let s2 = s2.trim().rsplit_once(' ').unwrap().1;
-    let pos2: u32 = s2.parse().unwrap();
-    // Status tuple: ( [(position, score); 2], current_player ) -> number_of_universes
-    let mut stack = Vec::new();
-    let mut winners = (0,0);
-    stack.push(([(pos1, 0), (pos2, 0)], 0));
-    // TODO: Trim this algorithm to trim repeated branches
-    // Depth-first search
-    while let Some((players, current)) = stack.pop() {
-        if players[current].1 > 20 {
-            if current % 2 == 0 {
-                winners.0 += 1;
-            } else {
-                winners.1 += 1;
-            }
-        } else {
-            for i in 1..=3 {
-                for j in 1..=3 {
-                    for k in 1..=3 {
-                        let pos = ((players[current].0 + i + j + k - 1) % 10) + 1;
-                        let score = players[current].1 + pos;
-                        let mut new_players = [(0,0); 2];
-                        new_players[1 - current] = players[1 - current];
-                        new_players[current] = (pos, score);
-                        stack.push((new_players, 1 - current));
+    let pos1 = s1.trim().rsplit_once(' ').unwrap().1.parse().unwrap();
+    let pos2 = s2.trim().rsplit_once(' ').unwrap().1.parse().unwrap();
+
+    let states1 = compute_universes(pos1);
+    let states2 = compute_universes(pos2);
+
+    let mut wins1 = 0;
+    for (turn, st1) in states1.iter() {
+        if let Some(st2) = states2.get(&(turn - 1)) {
+            for (&score1, u1) in st1 {
+                if score1 >= WIN_SCORE {
+                    for (&score2, u2) in st2 {
+                        if score2 < WIN_SCORE {
+                            wins1 += u1 * u2;
+                        }
                     }
                 }
             }
         }
     }
 
-    if winners.0 > winners.1 {
-        winners.0
+    let mut wins2 = 0;
+    for (turn, st2) in states2 {
+        if let Some(st1) = states1.get(&turn) {
+            for (score2, u2) in st2 {
+                if score2 >= WIN_SCORE {
+                    for (&score1, u1) in st1 {
+                        if score1 < WIN_SCORE {
+                            wins2 += u1 * u2;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if wins1 > wins2 {
+        wins1
     } else {
-        winners.1
+        wins2
     }
 }
 
@@ -143,9 +188,9 @@ fn example2() {
     assert_eq!(res, 444356092776315);
 }
 
-//#[test]
-//fn input2() {
-    //let input = fs::read_to_string("input.txt").unwrap();
-    //let res = run2(&input);
-    //assert_eq!(res,42);
-//}
+#[test]
+fn input2() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let res = run2(&input);
+    assert_eq!(res, 309196008717909);
+}
